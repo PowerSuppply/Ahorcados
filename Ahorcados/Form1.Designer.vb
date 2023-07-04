6e6b664ef2ad2a40c9e20c3b8eef1386d855e6d6
Jameees <108363737+PowerSuppply@users.noreply.github.com>
2023-07-04 17:59:09 -0600

    V.1

---
+++ Ahorcados/Form1.Designer.vb
@@ -0,0 +1,308 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Form1))
+        pierna1 = New PictureBox()
+        hiddenWord = New Label()
+        input = New TextBox()
+        verifyButton = New Button()
+        cabeza = New PictureBox()
+        brazo2 = New PictureBox()
+        brazo1 = New PictureBox()
+        cuerpo = New PictureBox()
+        pierna2 = New PictureBox()
+        vacio = New PictureBox()
+        PictureBox1 = New PictureBox()
+        Label2 = New Label()
+        errorMessage = New Label()
+        congrats = New Label()
+        topicLabel = New Label()
+        changeDaWorld = New PictureBox()
+        bro = New Label()
+        CType(pierna1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(cabeza, ComponentModel.ISupportInitialize).BeginInit()
+        CType(brazo2, ComponentModel.ISupportInitialize).BeginInit()
+        CType(brazo1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(cuerpo, ComponentModel.ISupportInitialize).BeginInit()
+        CType(pierna2, ComponentModel.ISupportInitialize).BeginInit()
+        CType(vacio, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(changeDaWorld, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' pierna1
+        ' 
+        pierna1.BackColor = Color.Transparent
+        pierna1.Image = CType(resources.GetObject("pierna1.Image"), Image)
+        pierna1.Location = New Point(115, 62)
+        pierna1.Name = "pierna1"
+        pierna1.Size = New Size(133, 194)
+        pierna1.SizeMode = PictureBoxSizeMode.Zoom
+        pierna1.TabIndex = 0
+        pierna1.TabStop = False
+        ' 
+        ' hiddenWord
+        ' 
+        hiddenWord.AutoSize = True
+        hiddenWord.FlatStyle = FlatStyle.System
+        hiddenWord.Font = New Font("SF Pro Display", 19F, FontStyle.Regular, GraphicsUnit.Point)
+        hiddenWord.ForeColor = Color.FromArgb(CByte(89), CByte(131), CByte(146))
+        hiddenWord.Location = New Point(133, 285)
+        hiddenWord.Name = "hiddenWord"
+        hiddenWord.Size = New Size(89, 31)
+        hiddenWord.TabIndex = 1
+        hiddenWord.Text = "_ _ _ _ _"
+        ' 
+        ' input
+        ' 
+        input.BackColor = Color.FromArgb(CByte(174), CByte(195), CByte(176))
+        input.BorderStyle = BorderStyle.FixedSingle
+        input.Font = New Font("SF Pro Display", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        input.ForeColor = Color.FromArgb(CByte(1), CByte(22), CByte(30))
+        input.Location = New Point(334, 167)
+        input.Name = "input"
+        input.Size = New Size(107, 27)
+        input.TabIndex = 2
+        input.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' verifyButton
+        ' 
+        verifyButton.BackColor = Color.Transparent
+        verifyButton.FlatStyle = FlatStyle.Flat
+        verifyButton.Font = New Font("SF Pro Display", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        verifyButton.ForeColor = Color.FromArgb(CByte(174), CByte(195), CByte(176))
+        verifyButton.Location = New Point(469, 167)
+        verifyButton.Name = "verifyButton"
+        verifyButton.Size = New Size(87, 28)
+        verifyButton.TabIndex = 3
+        verifyButton.Text = "Verificar"
+        verifyButton.TextAlign = ContentAlignment.TopCenter
+        verifyButton.UseVisualStyleBackColor = False
+        ' 
+        ' cabeza
+        ' 
+        cabeza.BackColor = Color.Transparent
+        cabeza.Image = CType(resources.GetObject("cabeza.Image"), Image)
+        cabeza.Location = New Point(115, 62)
+        cabeza.Name = "cabeza"
+        cabeza.Size = New Size(133, 194)
+        cabeza.SizeMode = PictureBoxSizeMode.Zoom
+        cabeza.TabIndex = 4
+        cabeza.TabStop = False
+        ' 
+        ' brazo2
+        ' 
+        brazo2.BackColor = Color.Transparent
+        brazo2.Image = CType(resources.GetObject("brazo2.Image"), Image)
+        brazo2.Location = New Point(115, 62)
+        brazo2.Name = "brazo2"
+        brazo2.Size = New Size(133, 194)
+        brazo2.SizeMode = PictureBoxSizeMode.Zoom
+        brazo2.TabIndex = 5
+        brazo2.TabStop = False
+        ' 
+        ' brazo1
+        ' 
+        brazo1.BackColor = Color.Transparent
+        brazo1.Image = CType(resources.GetObject("brazo1.Image"), Image)
+        brazo1.Location = New Point(115, 62)
+        brazo1.Name = "brazo1"
+        brazo1.Size = New Size(133, 194)
+        brazo1.SizeMode = PictureBoxSizeMode.Zoom
+        brazo1.TabIndex = 6
+        brazo1.TabStop = False
+        ' 
+        ' cuerpo
+        ' 
+        cuerpo.BackColor = Color.Transparent
+        cuerpo.Image = CType(resources.GetObject("cuerpo.Image"), Image)
+        cuerpo.Location = New Point(115, 62)
+        cuerpo.Name = "cuerpo"
+        cuerpo.Size = New Size(133, 194)
+        cuerpo.SizeMode = PictureBoxSizeMode.Zoom
+        cuerpo.TabIndex = 7
+        cuerpo.TabStop = False
+        ' 
+        ' pierna2
+        ' 
+        pierna2.BackColor = Color.Transparent
+        pierna2.Image = CType(resources.GetObject("pierna2.Image"), Image)
+        pierna2.Location = New Point(115, 62)
+        pierna2.Name = "pierna2"
+        pierna2.Size = New Size(133, 194)
+        pierna2.SizeMode = PictureBoxSizeMode.Zoom
+        pierna2.TabIndex = 8
+        pierna2.TabStop = False
+        ' 
+        ' vacio
+        ' 
+        vacio.BackColor = Color.Transparent
+        vacio.Location = New Point(115, 62)
+        vacio.Name = "vacio"
+        vacio.Size = New Size(133, 194)
+        vacio.SizeMode = PictureBoxSizeMode.Zoom
+        vacio.TabIndex = 9
+        vacio.TabStop = False
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.BackColor = Color.Transparent
+        PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), Image)
+        PictureBox1.Location = New Point(32, 30)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(285, 337)
+        PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox1.TabIndex = 10
+        PictureBox1.TabStop = False
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("SF UI Display SemBd", 20.25F, FontStyle.Bold, GraphicsUnit.Point)
+        Label2.ForeColor = Color.Coral
+        Label2.Location = New Point(381, 97)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(146, 32)
+        Label2.TabIndex = 11
+        Label2.Text = "Ahorcados"
+        ' 
+        ' errorMessage
+        ' 
+        errorMessage.AutoSize = True
+        errorMessage.ForeColor = Color.Coral
+        errorMessage.Location = New Point(342, 210)
+        errorMessage.Name = "errorMessage"
+        errorMessage.Size = New Size(96, 15)
+        errorMessage.TabIndex = 12
+        errorMessage.Text = "Uy, estuvo cerca!"
+        errorMessage.TextAlign = ContentAlignment.MiddleCenter
+        errorMessage.Visible = False
+        ' 
+        ' congrats
+        ' 
+        congrats.AutoSize = True
+        congrats.BorderStyle = BorderStyle.FixedSingle
+        congrats.FlatStyle = FlatStyle.Flat
+        congrats.Font = New Font("SF UI Display SemBd", 20.25F, FontStyle.Bold, GraphicsUnit.Point)
+        congrats.ForeColor = Color.Coral
+        congrats.Location = New Point(342, 262)
+        congrats.Name = "congrats"
+        congrats.Size = New Size(2, 34)
+        congrats.TabIndex = 13
+        congrats.Visible = False
+        ' 
+        ' topicLabel
+        ' 
+        topicLabel.AutoSize = True
+        topicLabel.Font = New Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point)
+        topicLabel.ForeColor = Color.Coral
+        topicLabel.Location = New Point(129, 328)
+        topicLabel.Name = "topicLabel"
+        topicLabel.Size = New Size(39, 19)
+        topicLabel.TabIndex = 14
+        topicLabel.Text = "topic"
+        topicLabel.TextAlign = ContentAlignment.MiddleLeft
+        ' 
+        ' changeDaWorld
+        ' 
+        changeDaWorld.BackColor = Color.Transparent
+        changeDaWorld.Image = CType(resources.GetObject("changeDaWorld.Image"), Image)
+        changeDaWorld.Location = New Point(94, 203)
+        changeDaWorld.Name = "changeDaWorld"
+        changeDaWorld.Size = New Size(205, 53)
+        changeDaWorld.SizeMode = PictureBoxSizeMode.Zoom
+        changeDaWorld.TabIndex = 15
+        changeDaWorld.TabStop = False
+        changeDaWorld.Visible = False
+        ' 
+        ' bro
+        ' 
+        bro.AutoSize = True
+        bro.BackColor = Color.Transparent
+        bro.Font = New Font("Segoe UI Emoji", 18.5F, FontStyle.Bold, GraphicsUnit.Point)
+        bro.ForeColor = Color.Coral
+        bro.Location = New Point(115, 97)
+        bro.Name = "bro"
+        bro.Size = New Size(137, 66)
+        bro.TabIndex = 16
+        bro.Text = "Bro is ded" & vbCrLf & "💀⚰️☠️"
+        bro.Visible = False
+        ' 
+        ' Form1
+        ' 
+        AcceptButton = verifyButton
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.FromArgb(CByte(12), CByte(45), CByte(59))
+        ClientSize = New Size(666, 389)
+        Controls.Add(bro)
+        Controls.Add(changeDaWorld)
+        Controls.Add(topicLabel)
+        Controls.Add(congrats)
+        Controls.Add(errorMessage)
+        Controls.Add(hiddenWord)
+        Controls.Add(Label2)
+        Controls.Add(vacio)
+        Controls.Add(cabeza)
+        Controls.Add(cuerpo)
+        Controls.Add(brazo1)
+        Controls.Add(brazo2)
+        Controls.Add(verifyButton)
+        Controls.Add(input)
+        Controls.Add(pierna1)
+        Controls.Add(pierna2)
+        Controls.Add(PictureBox1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(pierna1, ComponentModel.ISupportInitialize).EndInit()
+        CType(cabeza, ComponentModel.ISupportInitialize).EndInit()
+        CType(brazo2, ComponentModel.ISupportInitialize).EndInit()
+        CType(brazo1, ComponentModel.ISupportInitialize).EndInit()
+        CType(cuerpo, ComponentModel.ISupportInitialize).EndInit()
+        CType(pierna2, ComponentModel.ISupportInitialize).EndInit()
+        CType(vacio, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        CType(changeDaWorld, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents pierna1 As PictureBox
+    Friend WithEvents hiddenWord As Label
+    Friend WithEvents input As TextBox
+    Friend WithEvents verifyButton As Button
+    Friend WithEvents cabeza As PictureBox
+    Friend WithEvents brazo2 As PictureBox
+    Friend WithEvents brazo1 As PictureBox
+    Friend WithEvents cuerpo As PictureBox
+    Friend WithEvents pierna2 As PictureBox
+    Friend WithEvents vacio As PictureBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents errorMessage As Label
+    Friend WithEvents congrats As Label
+    Friend WithEvents topicLabel As Label
+    Friend WithEvents changeDaWorld As PictureBox
+    Friend WithEvents bro As Label
+End Class
---
+++ Ahorcados/Form1.vb
@@ -0,0 +1,123 @@
+﻿Imports System.Random
+
+Public Class Form1
+    Public words As String() = {"perro", "gato", "vaca", "paloma", "caballo", "oveja", "jirafa", "ballena", "canguro", "oe", "causa", "mano", "jatear", "jamear", "palta", "atracar", "pepa", "peru", "rusia", "australia", "portugal", "argentina", "panama", "chile", "mexico", "alemania", "james", "aaron", "ingrid", "bayron", "sol", "kimberly", "jordan", "rodolfo", "alejandro", "pamela", "josue", "nelson", "jeremy", "bernal", "bryton"}
+    Public disfavour As String() = {"Uy, que cerca", "CASI, CASI", "Frio, frio", "Sigue, papi, sigue", "Tu puedes mi kong", "Nooo pampu"}
+    Public repeated As String() = {}
+    Dim rand As New Random()
+    Dim index As Integer = rand.Next(0, words.Length) 'Elije una palabra por index aleatorio
+    Dim chosenWord As String = words(index) ' Guarda la palabra en una variable
+    Dim dashes As String = New String("")
+    Dim sentinel As Integer ' Variable sentinela a usar para controlar los errores
+    Private Sub loadGame()
+        bro.Visible = False
+        sentinel = 0
+        pierna2.Visible = True
+        changeDaWorld.Visible = False
+        congrats.Visible = False
+        errorMessage.Text = ""
+        index = 0
+        index = rand.Next(0, words.Length)
+        chosenWord = words(index)
+        dashes = ""
+        For i As Integer = 0 To chosenWord.Length - 1
+            dashes &= "_ "
+        Next
+        hiddenWord.Text = dashes ' muestra la palabra con las letras ocultas
+        If index <= 8 Then
+            topicLabel.Text = "Animales"
+        ElseIf index <= 16 And index > 8 Then
+            topicLabel.Text = "Jergas peruanas"
+        ElseIf index <= 25 And index > 16 Then
+            topicLabel.Text = "Paises"
+        Else
+            topicLabel.Text = "Nombres de personas"
+        End If
+
+        vacio.Visible = True
+        cabeza.Visible = True
+        cuerpo.Visible = True
+        brazo1.Visible = True
+        brazo2.Visible = True
+        pierna1.Visible = True
+        pierna2.Visible = True
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        loadGame()
+    End Sub
+
+    Private Async Sub verifyButton_Click(sender As Object, e As EventArgs) Handles verifyButton.Click
+        Dim toggle As Boolean = False ' variable usada para condicionar el muestraje de textos de aliento
+        Dim counter As Integer = 0
+        errorMessage.Visible = False
+        For i As Integer = 0 To chosenWord.Length - 1 ' recorre la palabra
+            Dim temp() As Char = hiddenWord.Text.ToCharArray() ' guarda el texto de la palabra oculta como array de char para poder modificarlo
+            If input.Text = chosenWord(i) Then
+                temp(i * 2) = UCase(input.Text) 'si la letra ingresada esta en la palabra, modifica el array para mostrarla
+                hiddenWord.Text = New String(temp) ' actualiza el texto de "hiddenWord"
+                toggle = True
+            End If
+        Next
+        If toggle = False Then
+            sentinel += 1
+            errorMessage.Visible = True
+            index = rand.Next(0, disfavour.Length) 'elije un mensaje de error aleatorio
+            errorMessage.Text = disfavour(index)
+        End If
+        Select Case sentinel
+            Case 1
+                vacio.Visible = False
+            Case 2
+                cabeza.Visible = False
+            Case 3
+                cuerpo.Visible = False
+            Case 4
+                brazo1.Visible = False
+            Case 5
+                brazo2.Visible = False
+            Case 6
+                pierna1.Visible = False
+            Case 7
+                congrats.Visible = True
+                changeDaWorld.Visible = True
+                congrats.Text = "YA PERDISTE, PAPI!"
+                dashes = ""
+                For i As Integer = 0 To chosenWord.Length - 1
+                    dashes &= UCase(chosenWord(i)) + " "
+                Next
+                hiddenWord.Text = dashes
+                Await Task.Delay(800)
+                pierna2.Visible = False
+                Await Task.Delay(700)
+                pierna2.Visible = True
+                Await Task.Delay(700)
+                pierna2.Visible = False
+                Await Task.Delay(500)
+                pierna2.Visible = True
+                Await Task.Delay(500)
+                pierna2.Visible = False
+                Await Task.Delay(300)
+                pierna2.Visible = True
+                Await Task.Delay(300)
+                pierna2.Visible = False
+                Await Task.Delay(200)
+                pierna2.Visible = False
+                bro.Visible = True
+                Await Task.Delay(300)
+                loadGame()
+        End Select
+        input.Text = ""
+        For i = 0 To hiddenWord.Text.Length - 1
+            If "_" = hiddenWord.Text(i) Then
+                counter += 1
+            End If
+        Next
+        If counter = 0 Then
+            congrats.Visible = True
+            congrats.Text = "FELICITACIONES!"
+            Await Task.Delay(700)
+            loadGame()
+        End If
+    End Sub
+End Class
